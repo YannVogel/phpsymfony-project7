0f0c14245b371dba6e8e71e7689ee8ac817862a7
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-07-09 11:46:52 +0200

    Update ProductController.php

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use App\Service\PaginationService;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
@@ -14,13 +18,27 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/", name="products_list", methods={"GET"})
-     * @param ProductRepository $productRepository
-     * @param SerializerInterface $serializer
+     * @Route("/{page<\d+>?1}", name="products_list", methods={"GET"})
+     * @param Request $request
+     * @param ProductRepository $repository
+     * @param PaginationService $paginationService
      * @return Response
      */
-    public function readProducts(ProductRepository $productRepository, SerializerInterface $serializer)
+    public function readProducts(Request $request, ProductRepository $repository, PaginationService $paginationService)
     {
-        return $this->json($productRepository->findAll(), 200, [], ['groups' => 'list']);
+        $limit = 10;
+        $page = $request->query->get('page');
+        $maxPage = $paginationService->getPages($repository, $limit);
+
+        if (is_null($page) || $page < 1) {
+            $page = 1;
+        } else if ($page > $maxPage) {
+            return $this->redirectToRoute('products_list', ['page' => 1], 302);
+        }
+
+        return $this->json(
+            $paginationService->paginateResults($repository, $page, $limit),
+            200, [],
+            ['groups' => 'list']);
     }
 }
