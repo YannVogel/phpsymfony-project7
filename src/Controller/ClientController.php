580771601ddbc30efe961d80f59a56774b048dbf
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-07-13 11:58:36 +0200

    Update ClientController
    Create ClientController->readUser( )

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Client;
+use App\Entity\User;
 use App\Repository\ClientRepository;
 use App\Repository\UserRepository;
 use App\Service\PaginationService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -17,6 +19,34 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ClientController extends AbstractController
 {
+    /**
+     * @Route("/{id}/users/{user_id}", name="client_user_detail", methods={"GET"})
+     * @Entity("user", expr="repository.find(user_id)")
+     * @param Client $client
+     * @param User $user
+     * @param UserRepository $repository
+     * @return JsonResponse
+     */
+    public function readUser(Client $client, User $user, UserRepository $repository)
+    {
+        $data = $repository->findBy(["client" => $client, "id" => $user->getId()]);
+
+        if (empty($data)) {
+            return $this->json(
+                [
+                'status' => 403,
+                'message' => 'Unauthorized to access this ressource.'
+                ],
+                403
+            );
+        }
+
+        return $this->json(
+            $data,
+            200, [],
+            ['groups' => 'detail']);
+    }
+
     /**
      * @Route("/{id}/users/{page<\d+>?1}", name="client_users_list", methods={"GET"})
      * @param Client $client
