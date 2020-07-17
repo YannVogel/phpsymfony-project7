72253c8f84e06a3ae7c6cd568d4b50a423742666
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-07-17 12:30:10 +0200

    Create SecurityService.php

---
+++ src/Service/SecurityService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class SecurityService extends AbstractController
+{
+    private int $clientId;
+    private int $pathId;
+
+    public function areClientIdsMatching() : bool
+    {
+        return $this->clientId !== $this->pathId ? false : true;
+    }
+
+    public function jsonToResponseIfIdsAreNotMatching() : JsonResponse
+    {
+        return $this->json(
+            [
+                'status' => 403,
+                'message' => 'Forbidden. The client ID indicated in the path is not your ID.',
+                'clientId' => 'Your client ID is ' . $this->clientId . '.'
+            ],
+            403
+        );
+    }
+
+    /**
+     * @return int
+     */
+    public function getClientId(): int
+    {
+        return $this->clientId;
+    }
+
+    /**
+     * @param int $clientId
+     * @return SecurityService
+     */
+    public function setClientId(int $clientId): SecurityService
+    {
+        $this->clientId = $clientId;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPathId(): int
+    {
+        return $this->pathId;
+    }
+
+    /**
+     * @param int $pathId
+     * @return SecurityService
+     */
+    public function setPathId(int $pathId): SecurityService
+    {
+        $this->pathId = $pathId;
+        return $this;
+    }
+
+
+}
