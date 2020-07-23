a9304381560477dca0bce70dc99be094239321d6
Flawxy <58270365+Flawxy@users.noreply.github.com>
2020-07-23 09:15:08 +0200

    Create JWTNotFoundListener.php

---
+++ src/EventListener/JWTNotFoundListener.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTNotFoundEvent;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class JWTNotFoundListener
+{
+    public function onJWTNotFound(JWTNotFoundEvent $event)
+    {
+        $event->setResponse(new JsonResponse([
+            'status'  => 403,
+            'message' => 'Missing credentials.',
+            ], 403)
+        );
+    }
+}
